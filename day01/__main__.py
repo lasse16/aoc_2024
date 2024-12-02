62731bb5aa9352f06591bc4a6b5ccba290b3a9b8
Lasse Haffke <lasse@haffke.me>
2024-12-02 12:01:44 +0100

    fix(formatting): apply ruff formatting

--- day01/__main__.py
+++ day01/__main__.py
@@ -1,21 +1,24 @@
 from collections import Counter
 
+
 def main():
     left_column, right_column = parse("input.txt")
     part1(left_column, right_column)
     part2(left_column, right_column)
 
+
 def parse(file):
     left_column = []
     right_column = []
 
-    with open(file, 'r') as file:
+    with open(file, "r") as file:
         for line in file.readlines():
             columns = line.split("   ")
             left_column.append(int(columns[0]))
             right_column.append(int(columns[1]))
 
-    return left_column,right_column
+    return left_column, right_column
+
 
 def part1(left, right):
     left.sort()
@@ -25,12 +28,14 @@ def part1(left, right):
         total_distance += abs(left[i] - right[i])
     print(total_distance)
 
+
 def part2(left, right):
     similarity_score = 0
     c = Counter(right)
     for id in left:
         similarity_score += c[id] * id
     print(similarity_score)
+
 
 if __name__ == "__main__":
     main()

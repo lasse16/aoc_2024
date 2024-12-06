2b0787ec301dae795205d4a15e0d0008e1b0c0bb
Lasse Haffke <lasse@haffke.me>
2024-12-06 13:26:57 +0100

    feat(day06): add solution for part2

--- day06/__main__.py
+++ day06/__main__.py
@@ -10,10 +10,10 @@ directions = [
 
 def main():
     grid, guard_start, max_x, max_y = parse("input.txt")
-    # rules, updates = parse("test-input.txt")
+    # grid, guard_start, max_x, max_y = parse("test-input.txt")
 
     print(f"Solution PART1: {part1(grid, guard_start,max_x,max_y)}")
-    # print(f"Solution PART2: {part2(grid, guard_start)}")
+    print(f"Solution PART2: {part2(grid, guard_start,max_x,max_y)}")
 
 
 def parse(file):
@@ -29,23 +29,14 @@ def parse(file):
                     grid[(x, y)] = False
                 if field == ".":
                     grid[(x, y)] = False
-    return grid, guard_start, x, y
+    # need to correct for last line
+    # in y direction \n is counted
+    # in x last line is not counted
+    return grid, guard_start, x + 1, y
 
 
 def part1(grid, guard_start, max_x, max_y):
-    visited_nodes = set()
-    current_direction_index = 0
-    current_position = guard_start
-    while current_position[0] in range(max_x) and current_position[1] in range(max_y):
-        visited_nodes.add(current_position)
-        potential_position = __tuple_add(
-            current_position, directions[current_direction_index]
-        )
-        if grid[potential_position]:
-            current_direction_index += 1
-            current_direction_index %= len(directions)
-        else:
-            current_position = potential_position
+    visited_nodes, _ = __move_guard(grid, guard_start, max_x, max_y)
     return len(visited_nodes)
 
 
@@ -56,8 +47,41 @@ def __tuple_add(tuple1, tuple2):
     return tuple(res)
 
 
-def part2(rules, updates):
-    pass
+def __move_guard(grid, guard_start, max_x, max_y):
+    visited_nodes = set()
+    visited_nodes_with_direction = defaultdict(set)
+    current_direction_index = 0
+    current_position = guard_start
+    while current_position[0] in range(max_x) and current_position[1] in range(max_y):
+        visited_nodes.add(current_position)
+        directions_node_was_visited_with = visited_nodes_with_direction[
+            current_position
+        ]
+        if current_direction_index in directions_node_was_visited_with:
+            return visited_nodes, True
+        directions_node_was_visited_with.add(current_direction_index)
+
+        potential_position = __tuple_add(
+            current_position, directions[current_direction_index]
+        )
+        if grid[potential_position]:
+            current_direction_index += 1
+            current_direction_index %= len(directions)
+        else:
+            current_position = potential_position
+    return visited_nodes, False
+
+# Correct solution but about 10s run time
+def part2(grid, guard_start, max_x, max_y):
+    default_path, _ = __move_guard(grid, guard_start, max_x, max_y)
+    loop_counter= 0
+    for potentially_looping_obstruction in default_path:
+        modified_grid = grid.copy()
+        modified_grid[potentially_looping_obstruction] = True
+        _, loop = __move_guard(modified_grid, guard_start, max_x, max_y)
+        if loop:
+            loop_counter += 1
+    return loop_counter
 
 
 if __name__ == "__main__":

02b19e68b7e6edce8ae9a1df3e1ca8bb5940230b
Lasse Haffke <lasse@haffke.me>
2024-12-25 17:11:03 +0100

    feat(day10): add part1

---
+++ day10/__main__.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+
+grid_steps = [
+    (0, -1),
+    (-1, 0),
+    (1, 0),
+    (0, 1),
+]
+
+
+def main():
+    args = parse("input.txt")
+    # args = parse("test-input.txt")
+
+    print(f"Solution PART1: {part1(args)}")
+    print(f"Solution PART2: {part2(args)}")
+
+
+def parse(file):
+    grid = defaultdict(int)
+    with open(file, "r") as file:
+        for x, line in enumerate(file.readlines()):
+            for y, c in enumerate(line.strip()):
+                grid[(x, y)] = int(c)
+    return grid, x + 1, y + 1
+
+
+def part1(args):
+    grid, max_x, max_y = args
+    sum_of_valid_trails = 0
+    for x in range(max_x):
+        for y in range(max_y):
+            start = (x, y)
+            if grid[start] == 0:
+                trailhead_score = get_valid_trail_count(grid, start)
+                print(f"trail-head: {start} with score: {trailhead_score}")
+                sum_of_valid_trails += trailhead_score
+    return sum_of_valid_trails
+
+
+def get_valid_trail_count(grid, start):
+    return len(set(get_trail_ends(grid, start)))
+
+
+def get_trail_ends(grid, start, current_num=0):
+    if grid[start] != current_num:
+        return []
+
+    if grid[start] == 9:
+        return [start]
+
+    trailheads = []
+    for direction in grid_steps:
+        trailheads += get_trail_ends(
+            grid, __comp_add(start, direction), current_num + 1
+        )
+    return trailheads
+
+
+def part2(args):
+    print(args)
+
+
+def __comp_add(tuple1, tuple2):
+    return (tuple1[0] + tuple2[0], tuple1[1] + tuple2[1])
+
+
+if __name__ == "__main__":
+    main()

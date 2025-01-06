0b84b055012d063f8355c3ebc4ad78077019f879
Lasse Haffke <lasse@haffke.me>
2025-01-06 01:42:54 +0100

    feat(day14): part2

--- day14/__main__.py
+++ day14/__main__.py
@@ -1,4 +1,5 @@
 from math import prod
+import sys
 import re
 
 
@@ -7,7 +8,7 @@ def main():
     # args = parse("test-input.txt")
 
     print(f"Solution PART1: {part1(args)}")
-    # print(f"Solution PART2: {part2(args)}")
+    print(f"Solution PART2: {part2(args)}")
 
 
 def parse(file):
@@ -24,18 +25,8 @@ def part1(args):
     max_x = 101
     max_y = 103
     step_count = 100
-    end_positions = []
-    for robot in args:
-        start_pos = robot[:2]
-        direction = robot[2:]
-        movements = tuple(map(lambda x: x * step_count, direction))
-        end_positions.append(
-            (
-                (start_pos[0] + movements[0]) % max_x,
-                (start_pos[1] + movements[1]) % max_y,
-            )
-        )
-
+    robots = args
+    end_positions = robots_at_step(robots, step_count, max_x, max_y)
     return prod(count_quadrants(end_positions, max_x, max_y))
 
 
@@ -71,7 +62,38 @@ def count_quadrants(positions, max_x, max_y):
 
 
 def part2(args):
-    print(args)
+    ##### ASSUMING ALL ROBOTS ARE IN THE SAME QUADRANT DURING THE EASTER EGG #####
+    max_x = 101
+    max_y = 103
+    min_security_factor = (-1, sys.maxsize)
+    for i in range(max_x * max_y):
+        step_count = i
+        end_positions = robots_at_step(args, step_count, max_x, max_y)
+
+        safety_factor = prod(count_quadrants(end_positions, max_x, max_y))
+        if safety_factor < min_security_factor[1]:
+            min_security_factor = (step_count, safety_factor)
+    print(
+        visualize(
+            robots_at_step(args, min_security_factor[0], max_x, max_y), max_x, max_y
+        )
+    )
+    return min_security_factor[0]
+
+
+def robots_at_step(robots, step_count, max_x, max_y):
+    end_positions = []
+    for robot in robots:
+        start_pos = robot[:2]
+        direction = robot[2:]
+        movements = tuple(map(lambda x: x * step_count, direction))
+        end_positions.append(
+            (
+                (start_pos[0] + movements[0]) % max_x,
+                (start_pos[1] + movements[1]) % max_y,
+            )
+        )
+    return end_positions
 
 
 if __name__ == "__main__":
